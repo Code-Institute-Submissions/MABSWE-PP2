2241866014b42e7d24d99c46d8d6d6210b789276
MABSWE <>
2023-12-13 18:08:33 +0000

    Add first two questions inside script js. give them a anwser with false or true boolean function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("test")
+const questions = [
+{
+    question: "What is the name of the main character in Finding Nemo?",
+    anwsers: [
+        { text:  "Marlin", correct: false},
+        { text:  "Dory", correct: false},
+        { text:  "Nemo", correct: true},
+        { text:  "Bruce", correct: false},
+    ]
+},
+{
+    question: "Which movie features the song Hakuna Matata?",
+    anwsers: [
+        { text:  "Aladdin", correct: false},
+        { text:  "The Lion King", correct: true},
+        { text:  "Frozen", correct: true},
+        { text:  "Moana", correct: false},
+    ]
+}
+]
